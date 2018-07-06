c96b57a54745eb417a198971b5c50ee574e88609
18021999 <40886135+18021999@users.noreply.github.com>
2018-07-06 14:31:50 +0530

    Create simpleudpserver.py

---
+++ simpleudpserver.py
@@ -0,0 +1,10 @@
+# networklab
+import socket
+UDP_IP_ADDRESS="127.0.0.1"
+UDP_PORT_NO=6100
+serverSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverSock.bind((UDP_IP_ADDRESS,UDP_PORT_NO))
+print("server is waiting")
+while True:
+     data,addr=serverSock.recvfrom(1024)
+     print("message:",data)
