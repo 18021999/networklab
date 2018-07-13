e56f28492ea3b5e415cea88c4afebad81985f4f5
18021999 <40886135+18021999@users.noreply.github.com>
2018-07-13 16:15:54 +0530

    Create tcp_sever.py

---
+++ tcp_sever.py
@@ -0,0 +1,15 @@
+from socket import *
+server_ip="10.1.24.35"
+server_port = 5000
+server_socket = socket(AF_INET,SOCK_STREAM)
+server_socket.bind((server_ip,server_port))
+server_socket.listen(1)
+print ("Welcome: The server is now ready to receive")
+connection_socket, address = server_socket.accept()
+while True:
+  sentence = connection_socket.recv(2048).decode()
+  print('>> ',sentence)
+  message = input(">> ")
+  connection_socket.send(message.encode())
+  if(message == 'q'):
+    connectionSocket.close()
